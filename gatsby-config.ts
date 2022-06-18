efc30335acb6484e4ad9d93e90875ede97580a08
kento <kento@toriwatari.bird>
2022-06-18 11:36:39 +0900

    ⚙ Setting ESLint

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,27 +2,24 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My Gatsby Site",
+    siteUrl: "https://www.yourdomain.tld"
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-typegen`,
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-typegen",
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "modalimages",
         path: `${__dirname}/src/images/modal`
       }
-    },
-  ],
+    }
+  ]
 }
 
 export default config
