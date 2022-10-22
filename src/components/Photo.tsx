1d283a426227ce159eeb007e3563abf3c634e4a9
kento <kento@toriwatari.bird>
2022-10-22 20:12:20 +0900

    ✨ #20 Photoコンポーネントを更新

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,25 +1,29 @@
 import React from "react"
 import { GatsbyImage } from "gatsby-plugin-image"
 
+import * as Styles from "../styles/Photo.module.scss"
+
 type Props = {
   showModal: Function
   setModalImage: Function
-  node: any
+  node: GatsbyTypes.IndexPageQuery["allImageSharp"]["edges"]
 }
 
-const Photo = ({ showModal, setModalImage, node }: Props, ref: any) => {
+const Photo = ({ showModal, setModalImage, node }: Props ) => {
   const handleClick = () => {
     showModal()
     setModalImage(node)
   }
 
   return (
-    <div>
+    <div
+      className={Styles.imgWrapper}
+      onClick={handleClick}
+    >
       <GatsbyImage
         image={node.node.gatsbyImageData}
         alt="hoge"
       />
-      <button onClick={() => handleClick()}>クリックミー</button>
     </div>
   )
 }
