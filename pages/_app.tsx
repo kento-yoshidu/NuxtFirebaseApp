26eb31f5ab47ba9064c5f0ac236288c00e5e3210
kento <kento@toriwatari.bird>
2023-02-03 20:57:08 +0900

    ✨ CSSに変更

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 
-import "./scss/global.scss"
+import "./scss/global.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
