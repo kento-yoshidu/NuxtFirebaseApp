51022850e0f1dd407638713ffa0ff183fe3cc288
kento <kento@toriwatari.bird>
2023-03-13 19:37:29 +0900

    ✨ Update Nav Component

--- pages/components/Nav.tsx
+++ pages/components/Nav.tsx
@@ -5,10 +5,10 @@ const Nav = () => (
     <h1 className={styles.navTitle}>Sample1</h1>
 
     <ul className={styles.navList}>
-      <li className={styles.navItem}><a href="#" className={styles.navLink}>HOME</a></li>
-      <li className={styles.navItem}><a href="#about" className={styles.navLink}>About</a></li>
-      <li className={styles.navItem}><a href="#article" className={styles.navLink}>Article</a></li>
-      <li className={styles.navItem}><a href="#photograph" className={styles.navLink}>Photograph</a></li>
+      <li><a href="#" className={styles.navLink}>HOME</a></li>
+      <li><a href="#about" className={styles.navLink}>About</a></li>
+      <li><a href="#article" className={styles.navLink}>Article</a></li>
+      <li><a href="#photograph" className={styles.navLink}>Photograph</a></li>
     </ul>
   </nav>
 )
