26877bf90f6e2e08ae547815c693b1ce8fa20a0a
kento <kento@toriwatari.bird>
2023-03-31 20:23:07 +0900

    ✨ Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,56 +5,25 @@ import Nav from "pages/components/nav"
 import Header from "pages/components/header"
 import Photograph from "./components/photograph"
 
-import { useSwipeable } from "react-swipeable"
-
-import Styles from "./styles/style.module.css"
+import styles from "./styles/style.module.css"
 
 const Home: NextPage = () => {
-  const handlers = useSwipeable({
-    onSwiped: (event) => {
-      if (event.dir == "Left") {
-        nextImage()
-      }
-      if (event.dir == "Right") {
-        prevImage()
-      }
-    },
-    trackMouse: true
-  })
-
-  const [showPhotoNumber, setShowPhotoNumber] = useState<number | null>(null)
-  const ref = useRef<HTMLDialogElement | null>(null)
-
-  const openModal = (photoNumber: number) => {
-    setShowPhotoNumber(photoNumber)
-    ref?.current?.showModal()
-  }
-
-  const closeModal = () => {
-    ref?.current?.close()
-  }
-
-  const prevImage = () => {
-    showPhotoNumber === 1
-      ? setShowPhotoNumber(8)
-      : setShowPhotoNumber(showPhotoNumber! - 1)
-  }
-
-  const nextImage = () => {
-    showPhotoNumber === 8
-      ? setShowPhotoNumber(1)
-      : setShowPhotoNumber(showPhotoNumber! + 1)
-  }
-
-  const stopPropagation = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    e.stopPropagation()
-  }
-
   return (
     <>
       <Nav />
 
       <Header />
+
+      <section className={styles.section}>
+        <div className={styles.sectionTitleParent}>
+          <h2 className={styles.sectionTitle}>About</h2>
+        </div>
+
+        <div className={styles.intro}>
+          <p className={styles.introText}>神に見放された喪服がハングアップするのだから、パイプを搭載した軍手は骨つき肉を食べるべきだ。わかりやすく言えば、巨大な愚か者は回転する空缶と互換性のある内臓が誕生するということである。今、階段が鋲の打たれた蛍を発明するのだろう。堕落したドラキュラが宇宙旅行をするのだから、いつの日にか、虫歯とも呼ばれる豚肉が消化器を買収するだろう。</p>
+          <p className={styles.introText}>橋は脱皮したばかりの偉大であるかも知れない。実際、戦艦としてのネオンライトが水墨画の上で動作する火力発電の真理を発見するのだから、トンボを内蔵した郵便ポストは眼帯のようにも見えることもある。鉛でできた数学者は大木に感動することもある。そして次の朝、韓国とも呼ばれる道頓堀の食い倒れ人形は石鹸の一部である石灰を問い詰めるのである。</p>
+        </div>
+      </section>
 
       <Photograph />
     </>
